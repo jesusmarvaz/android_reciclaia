337d5e915a1186f90b7195606ca93aaf54d95bb1
Jesús Martín Vázquez <jesusmarvaz@gmail.com>
2025-05-05 22:37:49 +0200

    saved work 23: added Waste class

---
+++ app/src/main/java/com/ingencode/reciclaia/data/repositories/IWasteRepository.kt
@@ -0,0 +1,8 @@
+package com.ingencode.reciclaia.data.repositories
+
+/**
+Created with ❤ by jesusmarvaz on 2025-05-05.
+*/
+
+interface IWasteRepository {
+}
---
+++ app/src/main/java/com/ingencode/reciclaia/domain/model/Waste.kt
@@ -0,0 +1,8 @@
+package com.ingencode.reciclaia.domain.model
+
+/**
+Created with ❤ by jesusmarvaz on 2025-05-05.
+*/
+
+class Waste {
+}
